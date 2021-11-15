09e2f9fc670d86af293772addf80a62bab3e2b36
Quentin7305 <93937329+Quentin7305@users.noreply.github.com>
2021-11-15 14:28:43 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,3 @@
-'''
-File: main.py
-Created Date: Friday August 27th 2021 - 02:35pm
-Author: Ammar Mian
-Contact: ammar.mian@univ-smb.fr
------
-Last Modified: Mon Aug 30 2021
-Modified By: Ammar Mian
------
-Copyright (c) 2021 Université Savoie Mont-Blanc
-'''
 import matplotlib.pyplot as plt
 from image import Image
 from reconnaissance import reconnaissance_chiffre, lecture_modeles
@@ -24,31 +13,9 @@ if __name__ == '__main__':
     # Lecture image et affichage
     #==============================================================================
     image = Image()
-    image.load(path_to_assets + 'test2.JPG')
+    image.load(path_to_assets + 'test4.JPG')
     image.display("Exemple d'image")
-
-    #==============================================================================
-    # Binarisation de l'image et affichage
-    #==============================================================================
     S = 70
-    image_binarisee = image.binarisation(S)
-    image_binarisee.display("Image binarisee")
-
-    #==============================================================================
-    # Localisation de l'image et affichage
-    #==============================================================================
-    image_localisee = image_binarisee.localisation()
-    image_localisee.display("Image localisee")
-
-    #==============================================================================
-    # Redimensionnement de l'image et affichage
-    #==============================================================================
-    image_resizee = image_localisee.resize(100, 500)
-    image_resizee.display("Image redimensionee")
-
-    #==============================================================================
-    # Lecture modeles et reconnaissance
-    #==============================================================================
     liste_modeles = lecture_modeles(path_to_assets)
     chiffre = reconnaissance_chiffre(image, liste_modeles, 70)
     print("Le chiffre reconnu est : ", chiffre)
